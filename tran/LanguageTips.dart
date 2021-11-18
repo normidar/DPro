614abf88ae764f962713f2f9424af8388720dc59
normidar <a@b.com>
2021-11-18 10:34:26 +0900

    u

--- tran/LanguageTips.dart
+++ tran/LanguageTips.dart
@@ -1,8 +1,11 @@
+import 'Tips/LanguageTip.dart';
+import 'Tips/ddart.dart';
+
 class LanguageTips {
-  static LanguageTip dart;
-  static LanguageTip python;
-  static LanguageTip java;
-  static LanguageTip swift;
-  static LanguageTip js;
-  
+  static LanguageTip dart = DDart();
+  // static LanguageTip python;
+  // static LanguageTip java;
+  // static LanguageTip swift;
+  // static LanguageTip js;
+
 }
